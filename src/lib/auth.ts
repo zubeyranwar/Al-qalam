019d8623179fedf083a083c5b82b8309913670ec
zubeyranwar <zubeyranwar@gmail.com>
2025-07-31 02:01:19 +0300

    feat: Create better auth instance

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,15 +1,22 @@
-import {betterAuth} from "better-auth"
-import {convexAdapter} from "@better-auth-kit/convex";
-import {ConvexHttpClient} from "convex/browser";
+import { convexAdapter } from "@convex-dev/better-auth";
+import { convex } from "@convex-dev/better-auth/plugins";
+import { betterAuth } from "better-auth";
+import { betterAuthComponent } from "../../convex/auth";
+import { type GenericCtx } from "../../convex/_generated/server";
 
-export const convexClient = new ConvexHttpClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
+const siteUrl = "http://localhost:3000";
 
-export const auth = betterAuth({
-    database: convexAdapter(convexClient),
-    socialProviders: {
-        github: {
-            clientId: process.env.NEXT_PUBLIC_GITHUB_CLIENT_ID!,
-            clientSecret: process.env.NEXT_PUBLIC_GITHUB_CLIENT_SECRET!,
-        }
-    },
-})
+export const createAuth = (ctx: GenericCtx) =>
+    betterAuth({
+        baseURL: siteUrl,
+        database: convexAdapter(ctx, betterAuthComponent),
+        socialProviders: {
+            github: {
+                clientId: process.env.NEXT_PUBLIC_GITHUB_CLIENT_ID!,
+                clientSecret: process.env.NEXT_PUBLIC_GITHUB_CLIENT_SECRET!,
+            }
+        },
+        plugins: [
+            convex(),
+        ],
+    });
