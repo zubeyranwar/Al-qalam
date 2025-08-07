106fa6767ad2676132687366ef14b4da9912aab4
zubeyranwar <zubeyranwar@gmail.com>
2025-08-08 01:10:49 +0300

    feat: Update using convex better auth integration

--- src/app/api/auth/[...all]/route.ts
+++ src/app/api/auth/[...all]/route.ts
@@ -1,4 +1,3 @@
-import { auth } from "@/lib/auth"; // path to your auth file
-import { toNextJsHandler } from "better-auth/next-js";
+import { nextJsHandler } from "@convex-dev/better-auth/nextjs";
 
-export const { POST, GET } = toNextJsHandler(auth);
+export const { GET, POST } = nextJsHandler();
