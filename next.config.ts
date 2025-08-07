a2b8151de8fba8f2bfd22991cb68b031ee984bb5
zubeyranwar <zubeyranwar@gmail.com>
2025-08-08 01:07:18 +0300

    chore: allow external images from GitHub and Edge Store

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,19 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'avatars.githubusercontent.com',
+                pathname: '/**',
+            },
+            {
+                protocol: "https",
+                hostname: "files.edgestore.dev",
+            },
+        ]
+    }
 };
 
 export default nextConfig;
