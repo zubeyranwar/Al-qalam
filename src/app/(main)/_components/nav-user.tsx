b00b7e48da201a1e286d001ef8bfc186b37897da
zubeyranwar <zubeyranwar@gmail.com>
2025-08-08 01:05:38 +0300

    feat: Use new convex-better auth integration

--- src/app/(main)/_components/nav-user.tsx
+++ src/app/(main)/_components/nav-user.tsx
@@ -1,29 +1,32 @@
 import {
     DropdownMenu,
-    DropdownMenuContent, DropdownMenuItem,
+    DropdownMenuContent,
+    DropdownMenuItem,
     DropdownMenuLabel,
     DropdownMenuSeparator,
     DropdownMenuTrigger
 } from "@/components/ui/dropdown-menu";
 import Image from "next/image";
-import {ChevronsUpDown, Menu} from "lucide-react";
+import {ChevronsUpDown} from "lucide-react";
 import {useAuth} from "@/hooks/use-auth";
+import {useRouter} from "next/navigation";
 
 export default function NavUser() {
+    const router = useRouter()
     const {session, logout} = useAuth()
 
-    const myLoader = () => {
-        return session?.user?.image || "";
+    const handleLogout = async () => {
+        await logout()
+        router.push("/");
     }
 
     return (
-        <div className="hover:bg-secondary py-2">
+        <div className="hover:bg-secondary py-2 px-4">
             <DropdownMenu>
                 <DropdownMenuTrigger asChild>
                     <div className="flex items-center gap-2 cursor-pointer">
                         <Image
-                            loader={myLoader}
-                            src={session?.user?.image || ""}
+                            src={session?.user?.image || "/placeholder.png"}
                             alt="profile picture"
                             width={24}
                             height={24}
@@ -35,14 +38,18 @@ export default function NavUser() {
                     </div>
                 </DropdownMenuTrigger>
 
-                <DropdownMenuContent align="end" className="w-[280px] space-4">
+                <DropdownMenuContent
+                    align="start"
+                    alignOffset={11}
+                    className="w-80"
+                    forceMount
+                >
                     <DropdownMenuLabel>
                         <h6 className="text-muted-foreground">{session?.user?.email}</h6>
                     </DropdownMenuLabel>
                     <DropdownMenuLabel>
                         <div className="flex items-center gap-2">
                             <Image
-                                loader={myLoader}
                                 src={session?.user?.image || ""}
                                 alt="profile picture"
                                 width={30}
@@ -50,18 +57,17 @@ export default function NavUser() {
                                 className="rounded-full object-contain"
                             />
 
-                            <h4 className="font-normal">{session?.user?.name} Qelem</h4>
+                            <h4 className="font-normal">{session?.user?.name}</h4>
                         </div>
                     </DropdownMenuLabel>
 
                     <DropdownMenuSeparator/>
-                    <DropdownMenuItem onClick={logout} className="text-muted-foreground">
+                    <DropdownMenuItem onClick={handleLogout} className="text-muted-foreground">
                         Logout
                     </DropdownMenuItem>
                 </DropdownMenuContent>
             </DropdownMenu>
 
-            <Menu />
         </div>
     )
 }
