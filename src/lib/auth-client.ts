46ca08af8fa04876a2f811c78a18264ae2d4a51e
zubeyranwar <zubeyranwar@gmail.com>
2025-08-23 19:20:50 +0300

    chore: Trimmable

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,8 +1,11 @@
-import { createAuthClient } from "better-auth/react";
-import { convexClient } from "@convex-dev/better-auth/client/plugins";
+import {createAuthClient} from "better-auth/react";
+import {convexClient, crossDomainClient} from "@convex-dev/better-auth/client/plugins";
+import {genericOAuthClient} from "better-auth/client/plugins";
 
 export const authClient = createAuthClient({
     plugins: [
         convexClient(),
+        crossDomainClient(),
+        genericOAuthClient()
     ],
 });
