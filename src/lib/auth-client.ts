e4cf400ac45de31875b5506fff457f4bdb1f7b85
zubeyranwar <zubeyranwar@gmail.com>
2025-09-06 17:14:10 +0300

    chore: remove base url from auth client

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -2,7 +2,6 @@ import {createAuthClient} from "better-auth/react";
 import {convexClient} from "@convex-dev/better-auth/client/plugins";
 
 export const authClient = createAuthClient({
-    baseURL: (process.env.NEXT_PUBLIC_SITE_URL || process.env.BETTER_AUTH_URL) as string,
     plugins: [
         convexClient(),
     ],
