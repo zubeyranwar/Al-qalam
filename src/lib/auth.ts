86891780f630b33e9571bde91e0fb5db275be199
zubeyranwar <zubeyranwar@gmail.com>
2025-09-08 23:56:01 +0300

    chore: Add base url specifically

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,19 +1,20 @@
-import { convexAdapter } from "@convex-dev/better-auth";
-import { convex } from "@convex-dev/better-auth/plugins";
-import { betterAuth } from "better-auth";
-import { betterAuthComponent } from "../../convex/auth";
-import { type GenericCtx } from "../../convex/_generated/server";
+import {convexAdapter} from "@convex-dev/better-auth";
+import {convex} from "@convex-dev/better-auth/plugins";
+import {betterAuth} from "better-auth";
+import {betterAuthComponent} from "../../convex/auth";
+import {type GenericCtx} from "../../convex/_generated/server";
 
 const siteUrl = "http://localhost:3000";
 
 export const createAuth = (ctx: GenericCtx) =>
     betterAuth({
-        baseURL: siteUrl,
+        baseURL: process.env.NEXT_PUBLIC_SITE_URL,
         database: convexAdapter(ctx, betterAuthComponent),
         socialProviders: {
             github: {
                 clientId: process.env.NEXT_PUBLIC_GITHUB_CLIENT_ID!,
                 clientSecret: process.env.NEXT_PUBLIC_GITHUB_CLIENT_SECRET!,
+                redirectURI: `${process.env.NEXT_PUBLIC_SITE_URL}/api/auth/callback/github`
             }
         },
         plugins: [
